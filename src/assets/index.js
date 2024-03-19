e4d295d51b0fa938c2f2abc02e81c4e8207cd173
Bianca Deblinger <biandeb96@gmail.com>
2024-03-19 02:14:07 -0300

    empiezo con el home, falta revisar foto

---
+++ src/assets/index.js
@@ -0,0 +1,1 @@
+export { default as TouristsImage } from './tourists.png'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
  content: [
   "./App.{js,jsx,ts,tsx}", 
   "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  // "./src/screens/**/*.{js,ts,jsx,tsx}",
-  // "./src/components/**/*.{js,ts,jsx,tsx}",
+  "./src/screens/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
 ],
     theme: {
       extend: {},
