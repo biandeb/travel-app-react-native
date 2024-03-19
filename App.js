4b60a0bbbc09106813bb0f3f9ece086e0c1f7efa
Bianca Deblinger <biandeb96@gmail.com>
2024-03-18 21:41:49 -0300

    comienzo de estructura de archivos iniciales del proyecto en React Native, con Expo Go y Tailwind css

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-blue-500 p-4">
+      <Text className="font-bold">Open up App.kkjs to start working on your apphh!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
